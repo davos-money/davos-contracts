923dcf6e376167044c7398bb069e665d9855a892
infbque <ivanoff.pg@yandex.ru>
2023-06-23 14:48:03 +0300

    deploy rate provider for testnet

--- scripts/oracles/add.stmatic.js
+++ scripts/oracles/add.stmatic.js
@@ -1,15 +1,6 @@
 let hre = require("hardhat");
 let {ethers, upgrades} = require("hardhat");
 
-function parseAddress(addressString) {
-    const buf = Buffer.from(addressString.replace(/^0x/, ''), 'hex');
-    if (!buf.slice(0, 12).equals(Buffer.alloc(12, 0))) {
-        return undefined;
-    }
-    const address = '0x' + buf.toString('hex', 12, 32); // grab the last 20 bytes
-    return ethers.utils.getAddress(address);
-}
-
 async function main() {
 
     // Signer
@@ -18,20 +9,43 @@ async function main() {
     let _nonce = initialNonce
 
     // Config
-    let {_cl_matic_usd, _stmatic_master_vault, _ratio_adapter, _stmatic, _stmatic_ratio_provider} = require(`./config_${hre.network.name}.json`);
+    let {
+        _cl_matic_usd,
+        _stmatic_master_vault,
+        _ratio_adapter,
+        _stmatic,
+        _stmatic_rate_provider,
+        _stmatic_fx_tunnel
+    } = require(`./config_${hre.network.name}.json`);
 
     // Fetching
     this.MasterVault = await hre.ethers.getContractFactory("MasterVault_V2");
     this.RatioAdapter = await hre.ethers.getContractFactory("RatioAdapter");
     this.StMaticOracle = await hre.ethers.getContractFactory("StMaticOracle");
+    this.FxRateProvider = await hre.ethers.getContractFactory("FxRateProvider");
 
-    let masterVaultV2 = await this.MasterVault.attach(_stmatic_master_vault);
     let ratioAdapter = await this.RatioAdapter.attach(_ratio_adapter);
 
     // Deployment
     console.log("Deploying...");
 
-    let stMaticOracle = await upgrades.deployProxy(this.StMaticOracle, [_cl_matic_usd, _stmatic, _stmatic_master_vault, _ratio_adapter], {initializer: "initialize", nonce: _nonce});
+    if (_stmatic_fx_tunnel && !_stmatic_rate_provider) {
+        console.log('Deploying our own RateProvider based on fx-portal channel');
+        const fxRateProvider = await this.FxRateProvider.deploy(_stmatic_fx_tunnel);
+        await fxRateProvider.deployed();
+        console.log("Verifying FxRateProvider...");
+        await hre.run("verify:verify", {
+            address: fxRateProvider.address,
+            constructorArguments: [_stmatic_fx_tunnel]
+        });
+        _stmatic_rate_provider = fxRateProvider.address;
+        console.log("FxRateProvider  : " + _stmatic_rate_provider);
+    }
+
+    let stMaticOracle = await upgrades.deployProxy(this.StMaticOracle, [_cl_matic_usd, _stmatic, _stmatic_master_vault, _ratio_adapter], {
+        initializer: "initialize",
+        nonce: _nonce
+    });
     _nonce += 1;
     await stMaticOracle.deployed();
     let stMaticOracleImp = await upgrades.erc1967.getImplementationAddress(stMaticOracle.address);
@@ -39,13 +53,14 @@ async function main() {
     console.log("Imp             : " + stMaticOracleImp);
 
     console.log("Setup contracts...");
-    await ratioAdapter.setToken(_stmatic, "", "", "getRate", true);
-    await ratioAdapter.setProviderForToken(_stmatic, _stmatic_ratio_provider);
+    await ratioAdapter.setToken(_stmatic, "", "", "getRate()", true);
+    await ratioAdapter.setProviderForToken(_stmatic, _stmatic_rate_provider);
 
     // Store Deployed Contracts
     const addresses = {
         _stMaticOracle: stMaticOracle.address,
         _stMaticOracleImp: stMaticOracleImp,
+        _rateProvider: _stmatic_rate_provider,
         _initialNonce: initialNonce
     }
 
