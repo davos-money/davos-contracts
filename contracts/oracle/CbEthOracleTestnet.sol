f62baaed3b7a5a3b4bb1975ee6a15cd6560d046f
infbque <ivanoff.pg@yandex.ru>
2023-06-27 15:29:25 +0300

    fix cbeth testnet oracle

--- contracts/oracle/CbEthOracleTestnet.sol
+++ contracts/oracle/CbEthOracleTestnet.sol
@@ -7,6 +7,6 @@ import "./extensions/CrossRateLstOracle.sol";
 contract CbEthOracleTestnet is CrossRateLstOracle {
     function initialize(AggregatorV3Interface _aggregatorAddress, address _cbETH, IRatioAdapter _ratioAdapter, IMasterVault _masterVault) external initializer {
         __LstOracle__init(_masterVault);
-        __CrossRateLstOracle__init(_aggregatorAddress, _rETH, _ratioAdapter);
+        __CrossRateLstOracle__init(_aggregatorAddress, _cbETH, _ratioAdapter);
     }
 }
