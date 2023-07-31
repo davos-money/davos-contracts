0e9465400fafa55627843ec5bc634b27f7968b24
davosloper <davosloper@gmail.com>
2023-07-31 17:19:03 +0500

    Edited configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -64,73 +64,73 @@ module.exports = {
             url: process.env.ETHEREUM_URL,
             chainId: 1,
             accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`],
-            gasPrice: parseInt(process.env.GAS_PRICE_2) || 'auto'
+            gasPrice: parseInt(process.env.GAS_PRICE_ETH) || 'auto'
         },
         goerli: {
             url: process.env.GOERLI_URL,
             chainId: 5,
             accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`],
-            gasPrice: parseInt(process.env.GAS_PRICE) || 'auto'
+            gasPrice: parseInt(process.env.GAS_PRICE_ETH) || 'auto'
         },
         polygon: {
             url: process.env.POLYGON_URL,
             chainId: 137,
             accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`],
-            gasPrice: parseInt(process.env.GAS_PRICE) || 'auto'
+            gasPrice: parseInt(process.env.GAS_PRICE_POL) || 'auto'
         },
         mumbai: {
             url: process.env.MUMBAI_URL,
             chainId: 80001,
             accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`],
-            gasPrice: parseInt(process.env.GAS_PRICE) || 'auto'
+            gasPrice: parseInt(process.env.GAS_PRICE_POL) || 'auto'
         },
         arbitrum: {
             url: process.env.ARBITRUM_URL,
             chainId: 42161,
             accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`],
-            gasPrice: parseInt(process.env.GAS_PRICE) || 'auto'
+            gasPrice: parseInt(process.env.GAS_PRICE_ARB) || 'auto'
         },
         arbitrumTestnet: {
             url: process.env.ARBITRUMGOERLI_URL,
             chainId: 421613,
             accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`],
-            gasPrice: parseInt(process.env.GAS_PRICE) || 'auto'
+            gasPrice: parseInt(process.env.GAS_PRICE_ARB) || 'auto'
         },
         optimism: {
             url: process.env.OPTIMISM_URL,
             chainId: 10,
             accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`],
-            gasPrice: parseInt(process.env.GAS_PRICE) || 'auto'
+            gasPrice: parseInt(process.env.GAS_PRICE_OPT) || 'auto'
         },
         optimismTestnet: {
             url: process.env.OPTIMISMGOERLI_URL,
             chainId: 420,
             accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`],
-            gasPrice: parseInt(process.env.GAS_PRICE) || 'auto'
+            gasPrice: parseInt(process.env.GAS_PRICE_OPT) || 'auto'
         },
         zkevm: {
             url: process.env.ZKEVM_URL,
             chainId: 1101,
             accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`],
-            gasPrice: parseInt(process.env.GAS_PRICE) || 'auto'
+            gasPrice: parseInt(process.env.GAS_PRICE_ZKE) || 'auto'
         },
         zkevmTestnet: {
             url: process.env.ZKEVMTESTNET_URL,
             chainId: 1442,
             accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`],
-            gasPrice: parseInt(process.env.GAS_PRICE) || 'auto'
+            gasPrice: parseInt(process.env.GAS_PRICE_ZKE) || 'auto'
         },
         bsc: {
             url: process.env.BSC_URL,
             chainId: 56,
             accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`],
-            gasPrice: parseInt(process.env.GAS_PRICE_1) || 'auto'
+            gasPrice: parseInt(process.env.GAS_PRICE_BSC) || 'auto'
         },
         bscTestnet: {
             url: process.env.BSCTESTNET_URL,
             chainId: 97,
             accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`],
-            gasPrice: parseInt(process.env.GAS_PRICE) || 'auto'
+            gasPrice: parseInt(process.env.GAS_PRICE_BSC) || 'auto'
         }
     },
 
